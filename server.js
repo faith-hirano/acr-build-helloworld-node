965fc043123466be85ede2f339621c14066ac390
faith-hirano <t_hirano@faith.co.jp>
2018-08-23 16:38:34 +0900

    added 16:38

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 80
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
+  response.write('Hello World 2018/08/23 16:38\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
